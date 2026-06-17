06d3a6b174134834b707029be021ccc1feac82e9
luna-candela <luna.candela@correounivalle.edu.co>
2026-06-17 00:03:37 -0500

    Add files via upload
    Subiendo paquete modelo

---
+++ src/modelo/Libro.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author User
+ */
+public class Libro {
+    
+    private int id;
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+    private String estado;
+    
+    public Libro (int id, String titulo,
+                  String autor, int anioPublicacion,
+                  String estado){
+    this.id = id;
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anioPublicacion = anioPublicacion;
+    this.estado = estado; 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
